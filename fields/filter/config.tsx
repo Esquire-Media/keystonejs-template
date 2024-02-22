a14ba2191d89e105d261f77fe7c18249519b1510
Isaac Jessup <ibjessup@gmail.com>
2024-02-22 18:26:04 -0500

    raqf list input
    Adds custom operators and a widget to handle IN and NOT IN filtering conditions

--- fields/filter/config.tsx
+++ fields/filter/config.tsx
@@ -1,9 +1,63 @@
-import React from "react";
-import type { Config, Widget, Operator } from "@react-awesome-query-builder/ui";
+import React from 'react';
+import { mergeDeep } from "@apollo/client/utilities";
+import type { Config, Operator, WidgetProps } from "@react-awesome-query-builder/ui";
 import { BasicConfig } from "@react-awesome-query-builder/ui";
 import ListInputWidget from "./widgets/ListInput";
 
 export default (init: Config = BasicConfig): Config =>
-  ({
-    ...init,
-  } as Config);
+  mergeDeep(init, {
+    operators: {
+      in: {
+        elasticSearchQueryType: "term",
+        jsonLogic: "in",
+        label: "Any In",
+        labelForFormat: "IN",
+        formatOp: (field, op, value: string, valueSrc, valueType, opDef) => {
+          console.log(field, op, value, valueSrc, valueType, opDef)
+          const formattedValues = value
+            .split(",")
+            .map((v) => `'${v}'`)
+            .join(", ");
+          return `${field} NOT IN (${formattedValues})`;
+        },
+        reversedOp: "not_in",
+        valueSources: ["value", "values"],
+        valueTypes: ["text"],
+      } as Operator,
+      not_in: {
+        elasticSearchQueryType: "term",
+        jsonLogic: "in",
+        label: "Not In",
+        labelForFormat: "NOT IN",
+        formatOp: (field, op, value: string, valueSrc, valueType, opDef) => {
+          const formattedValues = value
+            .split(",")
+            .map((v) => `'${v}'`)
+            .join(", ");
+          return `${field} NOT IN (${formattedValues})`;
+        },
+        reversedOp: "in",
+        valueSources: ["value", "values"],
+        valueTypes: ["text"],
+      } as Operator,
+    },
+    types: {
+      text: {
+        widgets: {
+          listInput: {
+            operators: [
+              "in",
+              "not_in",
+            ]
+          }
+        },
+      },
+    },
+    widgets: {
+      listInput: {
+        type: 'text',
+        valueSrc: 'value',
+        factory: (props: WidgetProps) => <ListInputWidget {...props} />,
+      },
+    }
+  });
