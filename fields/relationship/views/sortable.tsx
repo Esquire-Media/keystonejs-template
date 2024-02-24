5bd3bb01ab1ed58959da35f9731a5fa9c8522c2d
Isaac Jessup <ibjessup@gmail.com>
2024-02-24 10:22:57 -0500

    clean up

--- fields/relationship/views/sortable.tsx
+++ fields/relationship/views/sortable.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { WrapperProps } from "../wrapper";
 
 export default function Sortable(props: WrapperProps) {
-    console.log(props)
+    // console.log(props)
     return <div>asdf</div>
 }
