9713358975c6e43591a8bd76d27f2226a82ec44b
Isaac Jessup <ibjessup@gmail.com>
2024-02-24 10:11:56 -0500

    fields passthrough

--- fields/index.ts
+++ fields/index.ts
@@ -1,5 +1,42 @@
+import {
+  checkbox,
+  decimal,
+  file,
+  float,
+  integer,
+  bigInt,
+  image,
+  json,
+  password,
+  select,
+  text,
+  timestamp,
+  virtual,
+  calendarDay,
+  multiselect,
+} from "@keystone-6/core/fields";
+
 import filter from "./filter";
-import relationship from "./relationship"
+import relationship from "./relationship";
 import stars from "./stars";
 
-export { filter, relationship, stars };
+export {
+  checkbox,
+  decimal,
+  file,
+  float,
+  integer,
+  bigInt,
+  image,
+  json,
+  password,
+  select,
+  text,
+  timestamp,
+  virtual,
+  calendarDay,
+  multiselect,
+  filter,
+  relationship,
+  stars,
+};
