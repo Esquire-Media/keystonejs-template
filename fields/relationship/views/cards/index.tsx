38025821248dc5b655a187e86c5ad69cd29cc7a1
tjustsoup <tjustsoup@gmail.com>
2024-03-01 16:12:13 -0500

    onDragEnd basic functionality
    NOT COMPLETE
    Current issue is that the "currentIdsArrayWithFetchedItems" doesn't have the correct ordering.  Currently, this is not returning the "sort" (which will eventually be the "orderBy" variable) and that's required in order to make sure it's ordered properly.
    
    After fixing that, we also need to figure out how to set the items properly, because I don't think the "setItems" function works that way.  Though, maybe after getting the sorting working properly, this will work as intended as well.

--- fields/relationship/views/cards/index.tsx
+++ fields/relationship/views/cards/index.tsx
@@ -2,7 +2,6 @@
 /** @jsx jsx */
 
 import { Stack, Text, jsx } from "@keystone-ui/core";
-import "./styles.css";
 import { LoadingDots } from "@keystone-ui/loading";
 import { Ref, useEffect, useRef, useState } from "react";
 import { type ListMeta } from "@keystone-6/core/types";
@@ -116,6 +115,7 @@ export const Cards = React.forwardRef((props: CardProps, ref) => {
           setItems={setItems}
           selectedFields={selectedFields}
           client={client}
+          foreignList={foreignList}
           isLoadingLazyItems={isLoadingLazyItems}
           hideConnectItemsLabel={hideConnectItemsLabel}
           isMountedRef={isMountedRef}
