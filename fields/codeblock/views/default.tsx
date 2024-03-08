983374a8bb0f5ceaab6b68c027c599d5e913de86
Isaac Jessup <ibjessup@gmail.com>
2024-03-08 16:56:42 -0500

    language dropdown

--- fields/codeblock/views/default.tsx
+++ fields/codeblock/views/default.tsx
@@ -5,6 +5,9 @@ import Editor, { Monaco, OnChange } from "@monaco-editor/react";
 
 export default function View(props: WrapperProps) {
   const [lineHeight, setLineHeight] = useState<number>(19);
+  const [language, setLanguage] = useState<string>(
+    props.field.viewProps?.language || "json"
+  );
 
   function onMount(editor: editor.IStandaloneCodeEditor, monaco: Monaco) {
     setLineHeight(
@@ -21,25 +24,42 @@ export default function View(props: WrapperProps) {
     }
   };
   return (
-    <Editor
-      height={
-        lineHeight *
-        Math.min(
-          25,
-          Math.max(
-            5,
-            props.value.inner.kind === "null"
-              ? 5
-              : props.value.inner.value?.split(/\r\n|\r|\n/).length
+    <>
+      {!props.field.viewProps?.language ? (
+        <div>
+          <label htmlFor="language-select">Language: </label>
+          <select
+            id="language-select"
+            onChange={(event) => setLanguage(event.target.value)}
+            value={language}
+          >
+            <option value="json">JSON</option>
+            <option value="javascript">JavaScript</option>
+            <option value="html">HTML</option>
+            <option value="css">CSS</option>
+          </select>
+        </div>
+      ) : null}
+      <Editor
+        height={
+          lineHeight *
+          Math.min(
+            25,
+            Math.max(
+              5,
+              props.value.inner.kind === "null"
+                ? 5
+                : props.value.inner.value?.split(/\r\n|\r|\n/).length
+            )
           )
-        )
-      }
-      defaultLanguage={props.field.viewProps?.language}
-      value={props.value.inner.kind === "null" ? "" : props.value.inner.value}
-      onChange={onChange}
-      theme={props.field.viewProps?.theme || "vs-light"}
-      options={props.field.viewProps?.options}
-      onMount={onMount}
-    />
+        }
+        language={language}
+        value={props.value.inner.kind === "null" ? "" : props.value.inner.value}
+        onChange={onChange}
+        theme={props.field.viewProps?.theme || "vs-light"}
+        options={props.field.viewProps?.options}
+        onMount={onMount}
+      />
+    </>
   );
 }
